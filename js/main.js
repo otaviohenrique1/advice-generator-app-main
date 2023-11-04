9a09bcddc01844bddb171f1659e8b88d24687bdf
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-04 10:34:44 -0300

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 const slipId = document.getElementById("slip_id");
 const slip_advice = document.getElementById("slip_advice");
+const slip_dice = document.getElementById("slip_dice");
+
+let id = "";
+let advice = "";
 
 async function logMovies() {
   const response = await fetch("https://api.adviceslip.com/advice");
   const adviceslip = await response.json();
   const item = adviceslip.slip;
-  slipId.innerHTML = item.id;
-  slip_advice.innerHTML = item.advice;
+
+  id = item.id
+  advice = item.advice
+
+  slipId.innerHTML = id;
+  slip_advice.innerHTML = advice;
 }
 
-logMovies();
+if (id === "" && advice === "") {
+  slipId.innerHTML = "***";
+  slip_advice.innerHTML = "***************";
+}
+
+slip_dice.addEventListener("click", () => {
+  logMovies();
+})
+
+// const teste = { slip: { id: 222, advice: "Respect other people's opinions, even when they differ from your own." } }
+
+// slipId.innerHTML = teste.slip.id;
+// slip_advice.innerHTML = teste.slip.advice;
